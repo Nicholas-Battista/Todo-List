6dbcd897a0feb8aa7cd92d102b98cfaeee1045b2
Nicholas-Battista <battistan23@gmail.com>
2024-01-30 16:28:47 -0700

    finished structuring the new todo pop

---
+++ src/DOM.js
@@ -0,0 +1,7 @@
+import todoItem from "./class";
+
+function createTODO() {
+  const title = document.querySelector(".title");
+  const details = document.querySelector(".details");
+  const todo = new todoItem();
+}
