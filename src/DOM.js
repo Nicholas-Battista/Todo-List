c2d4fb0036fff48193553f723fa87f6de8ba5f98
Nicholas-Battista <battistan23@gmail.com>
2024-01-30 16:46:39 -0700

    began DOM module to read in info for todo objects

--- src/DOM.js
+++ src/DOM.js
@@ -1,7 +1,30 @@
 import todoItem from "./class";
 
 function createTODO() {
-  const title = document.querySelector(".title");
-  const details = document.querySelector(".details");
-  const todo = new todoItem();
+  const title = document.querySelector(".title").value;
+  const details = document.querySelector(".details").value;
+  const date = document.getElementById("date").value;
+  const priority = determinePriority();
+
+  const todo = new todoItem(title, details, date, priority);
+  console.log(todo);
 }
+
+function determinePriority() {
+  const low = document.getElementById("low");
+  const medium = document.getElementById("medium");
+  const high = document.getElementById("high");
+
+  if (low.checked) {
+    return "low";
+  } else if (medium.checked) {
+    return "medium";
+  } else if (high.checked) {
+    return "high";
+  }
+}
+
+const submitTodo = document.querySelector(".submit-todo");
+submitTodo.addEventListener("click", createTODO);
+
+export default createTODO;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import todoItem from "./class";
 import { generateTodo } from "./class";
+import createTODO from "./DOM";
 
 generateTodo("1", "2", "3", "4");
 generateTodo("1", "3", "3", "4");
