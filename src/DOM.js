f67bb3a9dcaac09317cdc692549d4ee16620e520
Nicholas-Battista <battistan23@gmail.com>
2024-01-30 17:02:10 -0700

    created function to display todos inside DOM.js

--- src/DOM.js
+++ src/DOM.js
@@ -10,6 +10,33 @@ function createTODO() {
   console.log(todo);
 }
 
+function displayTODOS(array, container) {
+  array.forEach((todo) => {
+    const div = document.createElement("div");
+    div.classList.add("todo-item");
+
+    div.appendChild(createCheckBox());
+    div.appendChild(createP(todo.title));
+    div.appendChild(createP(todo.description));
+    div.appendChild(createP(todo.date));
+    div.appendChild(createP(todo.priority));
+
+    container.appendChild(div);
+  });
+}
+
+function createCheckBox() {
+  const checkBox = document.createElement("input");
+  checkBox.type = "checkbox";
+  return checkBox;
+}
+
+function createP(string) {
+  const pElement = document.createElement("p");
+  pElement.textContent = string;
+  return pElement;
+}
+
 function determinePriority() {
   const low = document.getElementById("low");
   const medium = document.getElementById("medium");
@@ -28,3 +55,4 @@ const submitTodo = document.querySelector(".submit-todo");
 submitTodo.addEventListener("click", createTODO);
 
 export default createTODO;
+export { displayTODOS };
