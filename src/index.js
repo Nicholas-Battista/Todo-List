4e534f134317106db32959693570505ada2af291
Nicholas-Battista <battistan23@gmail.com>
2024-01-31 20:57:33 -0700

    major color changes some style changess

--- src/index.js
+++ src/index.js
@@ -6,6 +6,21 @@ let sections = {
   home: true,
   today: false,
   week: false,
+  update: function (section) {
+    if (section === "home") {
+      this.home = true;
+      this.today = false;
+      this.week = false;
+    } else if (section === "today") {
+      this.today = true;
+      this.home = false;
+      this.week = false;
+    } else {
+      this.week = true;
+      this.today = false;
+      this.home = false;
+    }
+  },
 };
 
 export { sections };
